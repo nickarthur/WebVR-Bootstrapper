9852c69d5e4e4a32c2dc0c22b9550700536d7575
Sean McBeth <Sean McBeth>
2016-05-17 19:05:30 -0400

    make sure cardboard only responds to left/right

--- src/CardboardVRDisplayPolyfill.js
+++ src/CardboardVRDisplayPolyfill.js
@@ -82,25 +82,28 @@
       currentPose = {
         timestamp: t,
         frameID: ++frameID,
-        orientation: new Float32Array(q)
+        orientation: q,
+        position: p
       };
     }
   }
 
   this.getEyeParameters = function (side) {
-    var dEye = side === "left" ? -1 : 1;
+    if (side === "left" || side === "right") {
+      var dEye = side === "left" ? -1 : 1;
 
-    return {
-      renderWidth: Math.floor(screen.width * devicePixelRatio / 2),
-      renderHeight: screen.height * devicePixelRatio,
-      offset: new Float32Array([dEye * 0.03, 0, 0]),
-      fieldOfView: {
-        upDegrees: 40,
-        downDegrees: 40,
-        leftDegrees: 40,
-        rightDegrees: 40
-      }
-    };
+      return {
+        renderWidth: Math.floor(screen.width * devicePixelRatio / 2),
+        renderHeight: screen.height * devicePixelRatio,
+        offset: new Float32Array([dEye * 0.03, 0, 0]),
+        fieldOfView: {
+          upDegrees: 40,
+          downDegrees: 40,
+          leftDegrees: 40,
+          rightDegrees: 40
+        }
+      };
+    }
   };
 
   this.getImmediatePose = function () {
